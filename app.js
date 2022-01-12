af4083ac03f79e23f3b90ea81346782a1c9665f7
NataliaErmeryd <natalia.ermeryd@outlook.com>
2022-01-12 11:47:06 +0100

    Finished day one

---
+++ app.js
@@ -0,0 +1,26 @@
+var username
+var password
+
+document.addEventListener("submit" , (event)=>{
+    console.log("Submitted")
+
+    username = document.getElementById("username").value 
+    password = document.getElementById("password").value 
+
+    const headers = new Headers()
+    headers.append("Accept" , "application/json")
+    headers.append("Authorization", btoa(username + ":" + password))
+
+    fetch("http://localhost:3000/" , { headers: headers}).then(function (response) {
+        if (response.ok) {
+            document.write("SUCCESSFULL LOGIN")
+        }
+        throw response;
+    }).then(function (data){
+        console.log(data);
+    }).catch(function (error) {
+    console.warn(error);
+});
+
+    event.preventDefault()
+})
