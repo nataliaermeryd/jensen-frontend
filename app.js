771a956db109bebf7deac27f2b217bec1f13d670
NataliaErmeryd <natalia.ermeryd@outlook.com>
2022-01-18 22:34:03 +0100

    Completed style tuesday w.3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener("submit" , (event)=>{
     let password = document.getElementById("password").value;
 
     let request = new XMLHttpRequest();
-    request.open("POST", "http://127.0.0.1:3000/authorize");
+    request.open("POST", "http://localhost:3000/authorize");
     //request.send(new FormData(formElement));
     console.log("Validated")
 
